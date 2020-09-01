a46c94d084e9af5c7dfc3658740a4927298069b6
Elev <Elev@TEB127022>
2020-09-01 10:57:19 +0200

    Fixade alla uppgifter

---
+++ C-arv/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseobject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in höjd");
+            int höjd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in bredd");
+            int bredd = int.Parse(Console.ReadLine());
+
+            Triangle tri = new Triangle(bredd, höjd);
+            Console.WriteLine("Omkrets: ", tri.Circumferance());
+            Console.WriteLine("Area: ", tri.Area());
+        }
+
+    }
+}
---
+++ C-arv/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseobject
+{
+    class Triangle : Shape
+    {
+        public Triangle(int width, int height) : base(width, height)
+        {
+        }
+        public int Area()
+        {
+            int area = (width * height) / 2;
+            return area;
+        }
+
+        public int Circumferance()
+        {
+            int Circ = (width + height) / 2;
+            return Circ;
+        }
+    }
+}
